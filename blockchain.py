4958504054c36497f8778c1ff5f99ac0ea822b39
Andreea Cozariuc <v-acozariuc@microsoft.com>
2024-07-01 16:48:19 +0300

    comments and doc strings

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,24 @@
+# this is a single line comment
+
 blockchain = []
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain. """
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append new value and last value to blockchain.
+    
+    Arguments:
+    :transaction_amount: The amount that should be added.
+    :last_transaction: The last blockchain transaction (default [1]).
+    """
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
+    """ Gets a new transaction amount from user input and returns it as a float. """
     return float(input('Your transaction amount, please: '))
 
 
